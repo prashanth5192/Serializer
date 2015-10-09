d94a9743a4c9b0ea24cc41c82c20a79db8bb6ec9
Jashwant Raj Gunasekaran <jashwant@net19pc186.cse.psu.edu>
2015-10-09 01:22:14 -0400

    added changes to all the files

--- dp_serial.c
+++ dp_serial.c
@@ -21,51 +21,61 @@ void Init_dp(int nphilosophers)
 	waiting_queue = Create_Queue(serial);
 }
 
-cond_t eat_cond(int phil_id)
+
+void *Eat(int phil_id, void *(*model_eat)())
 {
-	//int phil_id = (int)pthread_self();
-	printf("*** %d\n",phil_id);
-	int left,right;
-	if(phil_id > 0)
-		left = (phil_id-1)%N;
-	else
-		left = N-1;	   
-	right = (phil_id + 1)%N;
-	if(state[phil_id] == 1 && state[left]!=2 && state[right]!=2)
+	cond_t eat_cond()
 	{
-		state[phil_id] = 2;//going to eat
-		return 1;
+		//printf("*** %d\n",phil_id);
+		int left,right;
+		if(phil_id > 0)
+			left = (phil_id-1);
+		else
+			left = N-1;	   
+		right = (phil_id + 1)%N;
+		if(state[phil_id] == 1 && state[left]!=2 && state[right]!=2)
+		{
+			state[phil_id] = 2;//eat
+			return 1;
+		}
+		else
+			return 0;
 	}
-	else
-		return 0;
-}
+	void * eating()
+	{
+		model_eat(phil_id);
+		state[phil_id] = 0;//think aft eating
 
-cond_t think_cond(int phil_id)
-{
-	//int phil_id = (int)pthread_self();
-	if(state[phil_id] == 0)
-		return 1;
-	else
-		return 0; 
-}
+	}
 
-
-void *Eat(int phil_id, void (*model_eat)(int))
-{
 	Serial_Enter(serial);
-	Serial_Enqueue(serial, waiting_queue, &eat_cond);
 	state[phil_id] = 1; // hungry
-	Serial_Join_Crowd(serial, eating_crowd,  &(model_eat));
-	state[phil_id] = 0;
+	Serial_Enqueue(serial, waiting_queue, &eat_cond, 0);
+	Serial_Join_Crowd(serial, eating_crowd,  &eating);
 	Serial_Exit(serial);
-	pthread_exit(NULL);
+	//pthread_exit(NULL);
 }
 
-void *Think(int phil_id, void (*model_think)(int))
+void *Think(int phil_id, void* (*model_think)())
 {
+	cond_t think_cond()
+	{
+		if(state[phil_id] == 0)
+		{
+
+			return 1;
+		}
+		else
+			return 0; 
+	}
+	void *thinking()
+	{
+		model_think(phil_id);
+	}
+
 	Serial_Enter(serial);
-	Serial_Enqueue(serial, waiting_queue, &(think_cond));	
-	Serial_Join_Crowd(serial, thinking_crowd, &(model_think));
+	Serial_Enqueue(serial, waiting_queue, &think_cond, 0);	
+	Serial_Join_Crowd(serial, thinking_crowd, &thinking);
 	Serial_Exit(serial);
-	pthread_exit(NULL);
+	//pthread_exit(NULL);
 }
--- ds_serial.h
+++ ds_serial.h
@@ -1,2 +1,8 @@
-void Init_ds(int ncylinders); // ncylinders is total number of disk cylinders 
-int servicedseqno = Disk_Request(int cylinderno, void* model_request(), int &seekedcylinders); // cylinderno is requested cylinder. 3rd parameter should return number of cylinders seeked from previous request to service this request. Return value is the sequence number of service order of requests.
+#include"serial.h"
+
+serial_t *serial;
+queue_t *upQueue,*downQueue;
+crowd_t *disk_read,*users;
+
+void Init_ds(int ncylinders, int seektime); // ncylinders is total number of disk cylinders 
+int Disk_Request(int cylinderno, void* model_request(), int seekedcylinders[], int tid); // cylinderno is requested cylinder. 3rd parameter should return number of cylinders seeked from previous request to service this request. Return value is the sequence number of service order of requests.
